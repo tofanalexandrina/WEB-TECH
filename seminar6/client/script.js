52ece163b1991230c8247bab31c40b88cc433810
Tofan Alexandrina <tofanalexandrina22@stud.ase.ro>
2024-11-05 16:30:50 +0200

    seminar 6

---
+++ seminar6/client/script.js
@@ -0,0 +1,11 @@
+const getUsers=async()=>{
+    try{
+        const response=await fetch('http://127.0.0.1:3000/users');
+        const data=await response.json();
+
+        console.log(data);
+    }catch(error){
+        console.log(error);
+    }
+}
+getUsers();
---
+++ seminar6/users.js
@@ -0,0 +1,6 @@
+const users=[
+    {id:1, name:'Adrian', age:30},
+    {id:2, name:'Mihai', age:25},
+    {id:3, name:'Adrian', age:26}
+]
+module.exports=users;
