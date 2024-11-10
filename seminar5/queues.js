6208e3cf9e4c1a7ed1c0aafedd446096fbf5d0b3
Tofan Alexandrina <tofanalexandrina22@stud.ase.ro>
2024-11-10 15:30:59 +0200

    tema2

--- seminar5/queues.js
+++ seminar5/queues.js
@@ -2,6 +2,6 @@ console.log(1);
 //dupa promise se intoarce la setTimeOut
 setTimeout(()=>console.log(2), 0);
 
-//initial se uita la promise
+//initial se uita la promise - microtask
 Promise.resolve().then(()=>console.log(3));
 console.log(4);
